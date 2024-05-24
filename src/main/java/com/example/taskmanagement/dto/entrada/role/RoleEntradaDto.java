102d0e32a0ddc5e3550df1976ab30bc18620a465
Marisel725 <mariselchavez07@gmail.com>
2024-05-24 13:31:18 -0300

    actualizando la entidad Role

---
+++ src/main/java/com/example/taskmanagement/dto/entrada/role/RoleEntradaDto.java
@@ -0,0 +1,2 @@
+package com.example.taskmanagement.dto.entrada.role;public class RoleEntradaDto {
+}
---
+++ src/main/java/com/example/taskmanagement/dto/entrada/user/UserEntradaDtoModificacion.java
@@ -0,0 +1,2 @@
+package com.example.taskmanagement.dto.entrada.user;public class UserEntradaDtoModificacion {
+}
